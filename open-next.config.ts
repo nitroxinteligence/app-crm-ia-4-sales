ef5b2871fbc694708c394414d1d826a570a41862
Mateus M <nitroxinteligence@gmail.com>
2026-01-17 16:04:10 -0300

    chore: add open-next.config.ts to avoid interactive prompt during build

---
+++ open-next.config.ts
@@ -0,0 +1,11 @@
+
+import { defineCloudflareConfig } from '@opennextjs/cloudflare';
+
+export default defineCloudflareConfig({
+    /*
+     * The following options are optional.
+     * You can uncomment them to customize your configuration.
+     */
+    // incrementalCache: "dummy",
+    // wrap: true,
+});
